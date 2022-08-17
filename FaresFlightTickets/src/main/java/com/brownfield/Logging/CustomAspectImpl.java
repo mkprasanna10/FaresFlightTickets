e87229163ad59ecaf934897df91158f9f28a2694
mkprasanna10 <mkprasanna10@gmail.com>
2022-08-17 13:08:54 +0530

    Update CustomAspectImpl.java

--- FaresFlightTickets/src/main/java/com/brownfield/Logging/CustomAspectImpl.java
+++ FaresFlightTickets/src/main/java/com/brownfield/Logging/CustomAspectImpl.java
@@ -1,26 +1,42 @@
 package com.brownfield.Logging;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import com.brownfield.pss.fares.entity.Fare;
 
 @Aspect
 @Component
 public class CustomAspectImpl
 {
-	@Around("@Annotation(com.brownfield.Logging.Loggable)")
+	
+	@Before(value = "execution(public Fare findByFareByFlightNumberAndFlightDate(..))")
+	public void beforeAdvice(JoinPoint joinPoint, String FlightNumber, String FlightDate) {
+		System.out.println("Before method:" + joinPoint.getSignature());
+
+		System.out.println("Creating findByFareByFlightNumberAndFlightDate with FlightNumber - " + FlightNumber + " and FlightDate - " + FlightDate);
+	}
+
+	
+	/*@Around("@annotation(com.brownfield.Logging.Loggable)")
 	public Object myAroundRunMethod(ProceedingJoinPoint proceedingjoinpoint) throws Throwable
 	{
+		Object proceed = null;
 		try
 		{
-			proceedingjoinpoint.proceed();
+			long start = System.currentTimeMillis();
+		    proceed = proceedingjoinpoint.proceed();
+		    long executionTime = System.currentTimeMillis() - start;
+		    System.out.println(proceedingjoinpoint.getSignature() + " executed in " + executionTime + "ms");
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
-		return proceedingjoinpoint;
-	}
+		return proceed;
+	}*/
 	
 }
