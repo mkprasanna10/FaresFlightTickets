a19e71960aefd482bc68dec399e4acf6675f1a37
mkprasanna10 <mkprasanna10@gmail.com>
2022-08-17 13:09:31 +0530

    Update DemoApplication.java

--- FaresFlightTickets/src/main/java/com/brownfield/pss/fares/DemoApplication.java
+++ FaresFlightTickets/src/main/java/com/brownfield/pss/fares/DemoApplication.java
@@ -2,10 +2,16 @@ package com.brownfield.pss.fares;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.brownfield.Logging.Loggable;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class DemoApplication {
 
+	@Loggable
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
